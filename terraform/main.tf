2ba341939a6d344096176067f1dc3814c1fa0d6d
Mihamina Rakotovazaha <r.miham@yahoo.com>
2025-03-24 12:08:02 +0100

    feat: introduce VPCs

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,47 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_instance" "app_server" {
+####################
+### VPC resource ###
+####################
+
+resource "aws_vpc" "production" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "prod"
+  }
+}
+
+#####################
+### EC2 resources ###
+#####################
+
+resource "aws_instance" "app_fronted" {
   ami           = "ami-0359cb6c0c97c6607"
   instance_type = "t2.micro"
   key_name      = "gpac-admin-ssh"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "app_backend" {
+  ami           = "ami-0359cb6c0c97c6607"
+  instance_type = "t2.micro"
+  key_name      = "gpac-admin-ssh"
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_instance" "database" {
+  ami           = "ami-0359cb6c0c97c6607"
+  instance_type = "t2.micro"
+  key_name      = "gpac-admin-ssh"
+
+  tags = {
+    Name = "database"
   }
 }
