170ccab0e503e105491b6624d8fb5b992d19a597
Mihamina Rakotovazaha <r.miham@yahoo.com>
2025-03-24 12:08:02 +0100

    feat: introduce VPCs

---
+++ terraform/output.tf
@@ -0,0 +1,14 @@
+output "frontend_ip" {
+  description = "The public IP of the frontend Ec2 instance"
+  value       = aws_instance.app_fronted.public_ip
+}
+
+output "backend_ip" {
+  description = "The public IP of the backend Ec2 instance"
+  value       = aws_instance.app_fronted.public_ip
+}
+
+output "database_ip" {
+  description = "The public IP of the database EC2 instance"
+  value       = aws_instance.app_fronted.public_ip
+}
