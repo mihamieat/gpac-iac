f1526520c3642b5f10bd40042021cb829f399153
Mihamina Rakotovazaha <r.miham@yahoo.com>
2025-03-25 14:13:45 +0100

    fix: add database to public secyrity group

--- terraform/main.tf
+++ terraform/main.tf
@@ -198,8 +198,8 @@ resource "aws_instance" "app_backend" {
 resource "aws_instance" "database" {
   ami                    = "ami-0359cb6c0c97c6607"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_subnet.id
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
   key_name               = "gpac-admin-ssh"
 
   tags = {
